66800edccc6dc1deb54c39a3892f76ec24096ec7
Divya Prackash <divyaprackash@outlook.com>
2025-03-19 13:17:51 +0530

    Staging Table with relevant data

--- TinderSQLQuery.sql
+++ TinderSQLQuery.sql
@@ -34,6 +34,38 @@ Create table dbo.RAWTinderData(
 
 ALTER TABLE dbo.RAWTinderData ALTER COLUMN UserAge VARCHAR(50);
 
-exec sp_columns RAWTinderData;
+CREATE TABLE dbo.StageTinderData(
+	ID VARCHAR(50) NOT NULL PRIMARY KEY,
+	Sum_App_Opens INT,
+	No_Of_Days INT,
+	No_Of_Conversations INT,
+	LongestConversation INT,
+	LongestConversationInDays INT,
+	AverageConversationLength FLOAT,
+	AverageConversationLengthInDays FLOAT,
+	MedianConversationLength INT,
+	MedainConversationLengthInDays FLOAT,
+	No_Of_One_Message_Conversations INT,
+	PercentOfOneMessageConversations FLOAT,
+	No_Of_GhostingsAfterInitialMessage INT,
+	No_Of_Matches INT,
+	No_Of_Messages_Sent INT,
+	No_Of_Messages_Received INT,
+	Swipe_Likes INT,
+	Swipe_Passes INT,
+	BirthDate DATE,
+	CreateDate DATE,
+	AgeFilterMin INT,
+	AgeFilterMax INT,
+	Gender CHAR,
+	InterestedIn VARCHAR(10),
+	Instagram VARCHAR(5),
+	Spotify VARCHAR(5),
+	JobTitle VARCHAR(40),
+	UserAge INT
+);
 
-SELECT TOP(10) * FROM dbo.RAWTinderData ;
+
+Select TOP(10)* FROM dbo.StageTinderData
+
+ALTER TABLE dbo.StageTinderData ADD ApproxDuration AS DATEDIFF(MONTH,CreateDate,GETDATE())/12.0
